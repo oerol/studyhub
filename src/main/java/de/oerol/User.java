cb720532d1bcc7e8d7529c75130d0b1b37031ab2
Özgür Erol <erolxerolx@gmail.com>
2023-03-21 15:14:54 +0100

    added getters and setters for User

--- src/main/java/de/oerol/User.java
+++ src/main/java/de/oerol/User.java
@@ -1,22 +1,44 @@
 package de.oerol;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import java.time.LocalDate;
+
 @Entity
-@Table(name = "Students")
+@Table(name = "users")
 public class User {
     @Id
-    @Column(name = "student_id")
+    @Column(name = "id")
+    @JsonProperty("id")
     private Long id;
 
-    @Column(name = "first_name")
+    @Column(name = "username")
+    @JsonProperty("username")
     private String username;
 
-    @Column(name = "last_name")
+    @Column(name = "email")
+    @JsonProperty("email")
+    private String email;
+
+    @Column(name = "password")
+    @JsonProperty("password")
     private String password;
+
+    @Column(name = "first_name")
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    @JsonProperty("last_name")
+    private String lastName;
+
+    @Column(name = "birthdate")
+    @JsonProperty("birthdate")
+    private LocalDate birthDate;
 
     public Long getId() {
         return id;
@@ -34,11 +56,43 @@ public class User {
         this.username = username;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
     }
 }
